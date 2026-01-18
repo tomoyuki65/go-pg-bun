0a252d597556de2f6bd895758ea50592e8c6dfe3
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2026-01-18 19:32:47 +0900

    init

--- src/main.go
+++ src/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println("Hello World !!")
+	// echoのルーター設定
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		// レスポンス結果の設定
+		res := map[string]string{
+			"message": "Hello World !!",
+		}
+
+		return c.JSON(http.StatusOK, res)
+	})
+
+	// ログ出力
+	slog.Info("start go-pg-atlas-bun")
+
+	// サーバー起動
+	e.Logger.Fatal(e.Start(":8080"))
 }
